0967e03970e2bba29a3bd16ed6042e510b4b8d48
Alex Bates <alexbates.dev@gmail.com>
2024-04-30 16:17:33 -0600

    Added a passive animation to the hero banner

--- src/components/FrontScreen.jsx
+++ src/components/FrontScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import MouseFollowEffect from './HexGlow';
 import IndexTopBar from './IndexTopBar';
 import { Typography, useTheme } from '@mui/material';
@@ -10,6 +10,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const FrontScreen = () => {
     const theme = useTheme();
     const [slabHover, setSlabHover] = React.useState(0);
+    const [isHovering, setIsHovering] = React.useState(false);
 
     const techCardStyle = (position, active) => {
         let style = {
@@ -29,6 +30,28 @@ const FrontScreen = () => {
         if (active) style['backgroundColor'] = theme.palette.background.alternate;
         return style;
     }
+
+    React.useEffect(() => {
+        const cycleIds = [0, 1, 2, 3];
+        let index = slabHover;
+        const intervalId = setInterval(() => {
+            if (!isHovering) {
+                setSlabHover(cycleIds[index]);
+                index = (index + 1) % cycleIds.length;
+            }
+        }, 2500);
+
+        return () => clearInterval(intervalId);
+    }, [isHovering]);
+
+    const handleMouseEnter = (id) => {
+        setIsHovering(true);
+        setSlabHover(id);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+    };
 
     return (
         <div
@@ -94,9 +117,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.tertiary.main,
                     animation: 'floatBackend 5s ease-in-out infinite'
                 },
-                    slabHover === 2
+                    slabHover === 1 || slabHover == 3
                 )}>
-                    {slabHover === 2 && <img src='mysql.webp' style={{
+                    {(slabHover === 1 || slabHover == 3) && <img src='mysql.webp' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -108,9 +131,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.tertiary.main,
                     animation: 'floatBackend 5s ease-in-out infinite'
                 },
-                    slabHover === 2
+                    slabHover === 1 || slabHover == 3
                 )}>
-                    {slabHover === 2 && <img src='dotnet-logo.svg' style={{
+                    {(slabHover === 1 || slabHover == 3) && <img src='dotnet-logo.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -121,9 +144,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.primary.main,
                     animation: 'floatFrontend 8s ease-in-out infinite',
                 },
-                    slabHover === 1
+                    slabHover === 0 || slabHover == 3
                 )}>
-                    {slabHover === 1 && <img src='node-js.svg' style={{
+                    {(slabHover === 0 || slabHover == 3) && <img src='node-js.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -134,9 +157,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.secondary.main,
                     animation: 'floatDevops 6s ease-in-out infinite',
                 },
-                    slabHover === 3
+                    slabHover === 2 || slabHover == 3
                 )}>
-                    {slabHover === 3 && <img src='aws.svg' style={{
+                    {(slabHover === 2 || slabHover == 3) && <img src='aws.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -147,9 +170,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.secondary.main,
                     animation: 'floatDevops 6s ease-in-out infinite',
                 },
-                    slabHover === 3
+                    slabHover === 2 || slabHover == 3
                 )}>
-                    {slabHover === 3 && <img src='docker-mark-blue.svg' style={{
+                    {(slabHover === 2 || slabHover == 3) && <img src='docker-mark-blue.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -160,9 +183,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.secondary.main,
                     animation: 'floatDevops 6s ease-in-out infinite',
                 },
-                    slabHover === 3
+                    slabHover === 2 || slabHover == 3
                 )}>
-                    {slabHover === 3 && <img src='k8s.svg' style={{
+                    {(slabHover === 2 || slabHover == 3) && <img src='k8s.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -173,9 +196,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.tertiary.main,
                     animation: 'floatBackend 5s ease-in-out infinite'
                 },
-                    slabHover === 2
+                    slabHover === 1 || slabHover == 3
                 )}>
-                    {slabHover === 2 && <img src='fastapi.svg' style={{
+                    {(slabHover === 1 || slabHover == 3) && <img src='fastapi.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -186,9 +209,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.tertiary.main,
                     animation: 'floatBackend 5s ease-in-out infinite'
                 },
-                    slabHover === 2
+                    slabHover === 1 || slabHover == 3
                 )}>
-                    {slabHover === 2 && <img src='mongodb.svg' style={{
+                    {(slabHover === 1 || slabHover == 3) && <img src='mongodb.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -199,9 +222,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.primary.main,
                     animation: 'floatFrontend 8s ease-in-out infinite',
                 },
-                    slabHover === 1
+                    slabHover === 0 || slabHover == 3
                 )}>
-                    {slabHover === 1 && <img src='next-js.svg' style={{
+                    {(slabHover === 0 || slabHover == 3) && <img src='next-js.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -212,9 +235,9 @@ const FrontScreen = () => {
                     backgroundColor: theme.palette.primary.main,
                     animation: 'floatFrontend 8s ease-in-out infinite',
                 },
-                    slabHover === 1
+                    slabHover === 0 || slabHover == 3
                 )}>
-                    {slabHover === 1 && <img src='react.svg' style={{
+                    {(slabHover === 0 || slabHover == 3) && <img src='react.svg' style={{
                         width: '80%',
                         aspectRatio: '1/1',
                     }} />}
@@ -231,22 +254,22 @@ const FrontScreen = () => {
                     <span style={{
                         display: slabHover == 0 ? 'inline-block' : 'none',
                     }}>
-                        Full-Stack Developer
+                        Front-End Developer
                     </span>
                     <span style={{
                         display: slabHover == 1 ? 'inline-block' : 'none',
                     }}>
-                        Front-End Developer
+                        Software Engineer
                     </span>
                     <span style={{
                         display: slabHover == 2 ? 'inline-block' : 'none',
                     }}>
-                        Software Engineer
+                        DevOps Engineer
                     </span>
                     <span style={{
                         display: slabHover == 3 ? 'inline-block' : 'none',
                     }}>
-                        DevOps Engineer
+                        Full-Stack Developer
                     </span>
                     <span style={{ display: 'none' }}>
                         Back-End Developer
@@ -279,8 +302,7 @@ const FrontScreen = () => {
                 alignItems: 'center',
                 flexGrow: 1,
             }}>
-                <img src={slabHover === 1 ? '/activeBlueSlab.png' : '/inactiveBlueSlab.png'}
-                    id='frontend-slab'
+                <div
                     style={{
                         width: '500px',
                         animation: 'floatFrontend 6s ease-in-out infinite',
@@ -289,33 +311,87 @@ const FrontScreen = () => {
                         zIndex: 3,
                         clipPath: 'polygon(45% 0, 55% 0, 100% 45%, 100% 60%, 55% 100%, 45% 100%, 0 60%, 0 45%)',
                     }}
-                    onMouseEnter={() => setSlabHover(1)}
-                    onMouseLeave={() => setSlabHover(0)}
-                />
-                <img src={slabHover === 2 ? '/activeGreenSlab.png' : '/inactiveBlueSlab.png'}
-                    id='backend-slab'
+                    onMouseEnter={() => handleMouseEnter(0)}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img src={'/activeBlueSlab.png'}
+                        style={{
+                            width: '500px',
+                            transition: 'opacity 0.3s ease-in-out',
+                            opacity: slabHover == 0 || slabHover == 3 ? 1 : 0,
+                        }}
+                    />
+                    <img src={'/inactiveBlueSlab.png'}
+                        style={{
+                            width: '500px',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            transition: 'opacity 0.3s ease-in-out',
+                            opacity: slabHover == 0 || slabHover == 3 ? 0 : 1,
+                        }}
+                    />
+                </div>
+                <div
                     style={{
                         width: '500px',
                         animation: 'floatBackend 4s ease-in-out infinite',
                         zIndex: 2,
-                        clipPath: 'polygon(45% 0, 55% 0, 100% 45%, 100% 60%, 55% 100%, 45% 100%, 0 60%, 0 45%)',
-                    }}
-                    onMouseEnter={() => setSlabHover(2)}
-                    onMouseLeave={() => setSlabHover(0)}
-                />
-                <img src={slabHover === 3 ? '/activeRedSlab.png' : '/inactiveBlueSlab.png'}
-                    id='devops-slab'
-                    style={{
-                        width: '500px',
-                        animation: 'floatDevops 5s ease-in-out infinite',
                         position: 'relative',
-                        top: '-150px',
-                        zIndex: 1,
                         clipPath: 'polygon(45% 0, 55% 0, 100% 45%, 100% 60%, 55% 100%, 45% 100%, 0 60%, 0 45%)',
                     }}
-                    onMouseEnter={() => setSlabHover(3)}
-                    onMouseLeave={() => setSlabHover(0)}
-                />
+                    onMouseEnter={() => handleMouseEnter(1)}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img src={'/activeGreenSlab.png'}
+                        style={{
+                            width: '500px',
+                            transition: 'opacity 0.3s ease-in-out',
+                            opacity: slabHover == 1 || slabHover == 3 ? 1 : 0,
+                        }}
+                    />
+                    <img src={'/inactiveBlueSlab.png'}
+                        style={{
+                            width: '500px',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            transition: 'opacity 0.3s ease-in-out',
+                            opacity: slabHover == 1 || slabHover == 3 ? 0 : 1,
+                        }}
+                    />
+                </div>
+                <div style={{
+                    width: '500px',
+                    animation: 'floatDevops 5s ease-in-out infinite',
+                    position: 'relative',
+                    top: '-150px',
+                    zIndex: 1,
+                    clipPath: 'polygon(45% 0, 55% 0, 100% 45%, 100% 60%, 55% 100%, 45% 100%, 0 60%, 0 45%)',
+                }}
+                    onMouseEnter={() => handleMouseEnter(2)}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <img src={'/activeRedSlab.png'}
+                        style={{
+                            width: '500px',
+                            zIndex: 1,
+                            transition: 'opacity 0.3s ease-in-out',
+                            opacity: slabHover == 2 || slabHover == 3 ? 1 : 0,
+                        }}
+                    />
+                    <img src={'/inactiveBlueSlab.png'}
+                        style={{
+                            width: '500px',
+                            zIndex: 1,
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            transition: 'opacity 0.3s ease-in-out',
+                            opacity: slabHover == 2 || slabHover == 3 ? 0 : 1,
+                        }}
+                    />
+                </div>
             </div>
         </div>
     );
